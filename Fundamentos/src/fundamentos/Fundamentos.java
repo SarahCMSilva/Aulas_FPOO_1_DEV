b9a162d58a98efe05e42bb530d87c8e1f48e5fc5
Sarah Christina <sarah.silva5@aluno.senai.br>
2023-07-28 10:58:17 -0300

    declarando string como objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,12 +14,13 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Declaração de variaveis
+        // Declaração de variaveis de tipos primitivos
         String nome = "Moises";
         int idade = 40;
         boolean estudando = true;
         double media = 5.5;
         
+        String bairro = new String("Centro");
     }
     
 }

1974a2758c5b3fecbc7bf47eb291b9bcc90b0f36
Brook <zeroescobar9@gmail.com>
2023-08-03 14:43:59 -0300

    Enviar atividade

--- Fundamentos/src/Atividade01/Atividade07.java
+++ Fundamentos/src/Atividade01/Atividade07.java
@@ -1,20 +1,47 @@
 
 package Atividade01;
 
+import java.util.Scanner;
+
 
 public class Atividade07 {
- public static void main(String[] args) {
-        int rs = somar(4,8,2,1);
-        System.out.println("A soma:" + rs);
-    }
-    
-    public static int somar(int... valores){
-        int resultado  = 0;
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a quantidade de Kw/h:");
+        double KwH = scanner.nextDouble();
+        System.out.println("Instalacao residencial (R), industrial (I) e comercial (C):");
+        String instacao = scanner.next();
+
+        scanner.close();
         
-        for (int i = 0; i < valores.length; i++) {
-            int valor = valores[i];
-            resultado += valor; 
+        if (instacao.equalsIgnoreCase("R")) {
+            if (KwH <= 500.0) {
+                double preco = KwH * 0.4;
+                System.out.println("Valor total: " + preco);
+            } else {
+                double preco = KwH * 0.65;
+                System.out.println("Valor total: " + preco);
+            }
         }
-        return resultado;
+        if (instacao.equalsIgnoreCase("I")) {
+            if (KwH <= 5000.0) {
+                double preco = KwH * 0.55;
+                System.out.println("Valor total: " + preco);
+            } else {
+                double preco = KwH * 0.60;
+                System.out.println("Valor total: " + preco);
+            }
+        }
+        if (instacao.equalsIgnoreCase("C")) {
+            if (KwH <= 1000.0) {
+                double preco = KwH * 0.55;
+                System.out.println("Valor total: " + preco);
+            } else {
+                double preco = KwH * 0.60;
+                System.out.println("Valor total: " + preco);
+            }
+        }
+        System.out.println("Quantidade de Kw/h: " + KwH);
+        System.out.println("Tipo de instalacao: " + instacao);
     }
 }

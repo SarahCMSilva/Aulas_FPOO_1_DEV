ad3ed0667945628485bbf803e8fd1dd17d37327c
Brook <zeroescobar9@gmail.com>
2023-08-03 14:33:13 -0300

    Enviar

--- Fundamentos/src/Atividade01/Atividade02.java
+++ Fundamentos/src/Atividade01/Atividade02.java
@@ -8,26 +8,26 @@ public class Atividade02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Recebe e lê o nome do produto
+      
         System.out.println("Digite o nome do produto:");
         String nomeProdto = scanner.nextLine();
 
-        // Recebe e lê o valor do produto
+     
         System.out.println("Digite o valor do produto:");
         double valorProdto = scanner.nextDouble();
 
-        // Recebe e lê a quantidade do produto
+       
         System.out.println("Digite a quantidade do produto:");
         int qtdadeProdto = scanner.nextInt();
 
-        // Calcular o valor total
+       
         double valorTotal = valorProdto * qtdadeProdto;
 
-        // Recebe a forma de pagamento
+      
         System.out.println("Digite a forma de pagamento C(credito) ou D(debito):");
         String formaPagamento = scanner.next();
         
-        // calcula os descontos
+      
         if (formaPagamento.equalsIgnoreCase("D")) {
             if (valorTotal <= 100.0) {
                 valorTotal = valorTotal - (valorTotal * 0.05) ;
@@ -46,13 +46,13 @@ public class Atividade02 {
             System.out.println("Forma de pagamento inválida");
         }
 
-        // Exibe as informações
+     
         System.out.println("Nome do produto:" + nomeProdto);
         System.out.println("Valor do produto:" + valorProdto);
         System.out.println("Quantidade:" + qtdadeProdto);
         System.out.println("Valor total:" + valorTotal);
 
-        // Fecha o scanner
+      
         scanner.close();
     }
     

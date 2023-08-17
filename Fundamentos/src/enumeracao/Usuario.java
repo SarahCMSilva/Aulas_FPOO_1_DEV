1d8c9bb84e10a2d4c59fe107259575bbc78d44e5
SarahCMSilva <sarah.silva5@aluno.senai.br>
2023-08-17 10:20:09 -0300

    Aprendendo enumeracoes em java

---
+++ Fundamentos/src/enumeracao/Usuario.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Usuario {
+    private String nome;
+    private String email;
+    private boolean ativo;
+    private TipoUsuarioEnum tipo; 
+
+    public Usuario(String nome, String email, boolean ativo, TipoUsuarioEnum tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.ativo = ativo;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome=" + nome + ", email=" + email + ", ativo=" + ativo + ", tipo=" + tipo + '}';
+    }
+    
+}
